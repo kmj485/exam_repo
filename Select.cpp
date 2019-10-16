6968d2a09a86d1a7a8d9bbddf54c0813b8565f78
kmj485 <kmj@naver.com>
2019-10-16 19:03:11 +0900

    Select

---
+++ Select.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+using namespace std;
+// select
+
+int main(void)
+{
+	int arr[5] = { 4, 2, 3, 1, 5};
+
+	for (int i = 0; i < 5; i++) {
+		int min = 10;
+		int pivot;
+		for (int j = i; j < 5; j++) {
+			if ( min > arr[j]) {
+				min = arr[j];
+				pivot = j;
+			}
+		}
+		int tmp = arr[i];
+		arr[i] = arr[pivot];
+		arr[pivot] = tmp;
+	}
+	   
+	for (int i = 0; i < 5; i++) {
+		std::cout << arr[i] << " ";
+	}
+}
+
